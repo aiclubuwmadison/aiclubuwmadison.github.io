d1e40e6289c44f9d806bb63a6af66c6930d4dba2
Soham Dasgupta <sohamdasg@gmail.com>
2020-07-30 17:28:10 -0500

    Improved footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,6 +11,12 @@ const Footer = () => {
                 src={`${process.env.PUBLIC_URL}/images/logo_full.png`}
                 width={192}
               />
+              <div style={{float: "right"}}>
+                <div class="social">&#62220;</div>
+                <div class="social">&#62217;</div>
+                <div class="social">&#62232;</div>
+              </div>
+
             </Container>
           </nav>
         </footer>
