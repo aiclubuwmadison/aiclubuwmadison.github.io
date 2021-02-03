b3e9e0bd495ea95a935bcc563a79b7e99fb44b2c
Soham Dasgupta <sohamdasg@gmail.com>
2021-02-02 22:48:12 -0600

    Leadership section updated.

--- src/components/Leadership.js
+++ src/components/Leadership.js
@@ -15,86 +15,67 @@ const Leadership = () => {
                 <Grid container direction="row" justify="center" alignItems="center">
                     <Grid item xs>
                         <Portrait
-                            file="katherine.jpg"
+                            file="sarah.jpg"
                             title="Co-President"
+                            name="Sarah Worthington"
+                            link="https://www.linkedin.com/in/sarah-worthington/"
+                        />
+                    </Grid>
+                    <Grid item xs>
+                        <Portrait
+                            file="anniruddh.jpg"
+                            title="Co-President"
+                            name="Anniruddh Kumar" 
+                        />
+                    </Grid>
+                    <Grid item xs>
+                        <Portrait 
+                            file="declan.jpg"
+                            title="Club Advisor"
+                            name="Declan Campbell"
+                        />
+                    </Grid>
+                </Grid>
+                <Grid container direction="row" justify="center" alignItems="center">
+                    <Grid item xs>
+                        <Portrait
+                            file="katherine.jpg"
+                            title="Head of Research"
                             name="Katherine Fu"
                             link="https://www.linkedin.com/in/JiaruFu/"
                         />
                     </Grid>
                     <Grid item xs>
                         <Portrait
-                            file="atulya.jpg"
-                            title="Co-President"
-                            name="Atulya Reddy"
-                            link="https://www.linkedin.com/in/atulyareddy"
+                            file="taha.jpg"
+                            title="Head of Club Relations"
+                            name="Taha Sawar"
+                            link="https://www.linkedin.com/in/sawar/"
                         />
                     </Grid>
                     <Grid item xs>
-
                         <Portrait
                             file="soham.jpg"
-                            title="Vice President"
+                            title="Webmaster"
                             name="Soham Dasgupta"
                             link="https://www.linkedin.com/in/sohdas"
                         />
                     </Grid>                   
-                </Grid>
-                <Grid container direction="row" justify="center" alignItems="center">
-                    <Grid item xs>
-                        <Portrait 
-                            file="declan.jpg"
-                            title="Leader"
-                            name="Declan Campbell"
-                        />
                     </Grid>
-                    <Grid item xs>
-                        <Portrait
-                            file="jules.png"
-                            title="Leader"
-                            name="Jules Vigy"
-                            link="https://www.linkedin.com/in/julesvigy/"
-                        />
-                    </Grid>
+                    <Grid container direction="row" justify="center" alignItems="center">
                     <Grid item xs>
                         <Portrait
                             file="vishnu.jpg"
-                            title="Leader"
+                            title="Head of Finance"
                             name="Vishnu Yarlagadda"
                             link="https://www.linkedin.com/in/vishnu-yarlagadda-3b9b52183/"
                         />
                     </Grid>
-                </Grid>
-                <Grid container direction="row" justify="center" alignItems="center">
                     <Grid item xs>
                         <Portrait
                             file="alex.jpg"
-                            title="Leader"
+                            title="Head of Department Relations"
                             name="Alex Vesel"
-                        />
-                    </Grid>
-                    <Grid item xs>
-                        <Portrait
-                            file="adam.png"
-                            title="Leader"
-                            name="Adam Shedivy"
-                            link="https://www.linkedin.com/in/adam-shedivy-2619a1166/"
-                        />
-                    </Grid>
-                    <Grid item xs>
-                        <Portrait
-                            file="joash.jpg"
-                            title="Leader"
-                            name="Joash Shankar"
-                            link="https://www.linkedin.com/in/joash-shankar/"
-                        />
-                    </Grid>
-                </Grid>
-                <Grid container direction="row" justify="center" alignItems="center">
-                    <Grid item xs>
-                        <Portrait
-                            file="sarah.jpg"
-                            title="Graphic Designer"
-                            name="Sarah Worthington" 
                         />
                     </Grid>
                     <Grid item xs>
