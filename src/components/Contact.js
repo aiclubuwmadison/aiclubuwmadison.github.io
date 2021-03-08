5a317059783c4026f2800880e1e09edddfb0cfaf
Soham Dasgupta <sohamdasg@gmail.com>
2021-03-07 19:12:52 -0600

    embedded commets box

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Container, Typography } from '@material-ui/core';
+import { Container, Grid, Typography } from '@material-ui/core';
 import Header from './typographic/Header';
 import Subheader from './typographic/Subheader';
 import ArticleImage from './display/ArticleImage';
+import SubmissionBox from './SubmissionBox';
 
 const Contact = () => {
 
@@ -21,8 +22,17 @@ const Contact = () => {
                 &bull; <a className="link-text" target="_blank" href="mailto:aiclubuwmadison@gmail.com">Shoot us an email</a> if you have any questions or if you'd like to reach out to our leadership!
             </Subheader>
             <Subheader>
-                &bull; <a className="link-text" target="_blank" href="https://forms.gle/cKfuybyBQzv9vc8d8">Send us your thoughts</a> if there's anything else you want to say!
+                &bull; <a className="link-text" target="_blank" href="https://forms.gle/cKfuybyBQzv9vc8d8">Send us your thoughts</a> if there's anything else you want to say, either via the linked form or the submission fields below.
             </Subheader>
+            <br/>
+            <Header>
+                Students
+            </Header>
+            <Grid container xs>
+                <Grid item xs>
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdNX40SfpC-WOpe6rg56338ZD5-YaNeuTeb0B2Y5cVoZPW8tw/viewform?embedded=true" width="640" height="434" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">Loading…</iframe>
+                </Grid>
+            </Grid>
             <br></br>
             <ArticleImage file="sem3.jpg" caption="Our 2019 kickoff meeting"/>
         </Container>
