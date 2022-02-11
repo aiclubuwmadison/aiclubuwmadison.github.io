0f2860822dcfb3a957898c194e270fbf3e9c71fa
Ethan Wheeler <eswheeler@wisc.edu>
2022-02-10 18:31:36 -0600

    First build on Github Pages - using project url

---
+++ build/precache-manifest.b7dabbac65c9c82093c2aa658c0104e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9926a1604b5da2740837b2b3dca5a719",
+    "url": "/index.html"
+  },
+  {
+    "revision": "837e600c6c31f47b0fda",
+    "url": "/static/css/main.5f1890e9.chunk.css"
+  },
+  {
+    "revision": "434387f1978bae64264b",
+    "url": "/static/js/2.30e5e700.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.30e5e700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "837e600c6c31f47b0fda",
+    "url": "/static/js/main.d0950e91.chunk.js"
+  },
+  {
+    "revision": "8a3aebc74aa720c7b446",
+    "url": "/static/js/runtime-main.88eddbdb.js"
+  }
+]);
